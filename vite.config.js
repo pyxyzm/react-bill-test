9ac8a5171ddf24ef34d018edf8ade2e7d873d644
panyuxue <panyuxue@yonyou.com>
2026-01-06 20:35:09 +0800

    配置@路径

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'  // 配置 @ 别名指向 src 目录
+    }
+  },
 })
