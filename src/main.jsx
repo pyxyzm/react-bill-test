48caeb7dc7c816974655903118964f1a8c7ed428
panyuxue <panyuxue@yonyou.com>
2026-01-06 20:37:41 +0800

    设置mock，也就是json-server

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import sum from "@/test.jsx";
+
+
+
+const a = sum(1,2)
+console.log(a)
 
 createRoot(document.getElementById('root')).render(
     <App />
