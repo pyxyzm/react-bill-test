91c879987d3f9ac1b81b75e9851143ef4d142894
panyuxue <panyuxue@yonyou.com>
2026-01-08 00:36:56 +0800

    月底账单-点击切换

--- src/pages/Month/components/DayBill/index.jsx
+++ src/pages/Month/components/DayBill/index.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import './index.scss'
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
 import {billTypeToName} from "@/contants/index.jsx";
 
 const DailyBill = ({date,billList}) => {
@@ -19,12 +19,14 @@ const DailyBill = ({date,billList}) => {
             total: pay + income
         };
     }, [billList]);
+    //控制展开收起
+    const [expand, setExpand] = useState(false)
     return (
         <div className={classNames('dailyBill')}>
             <div className="header">
                 <div className="dateIcon">
                     <span className="date">{date}</span>
-                    <span className={classNames('arrow')}></span>
+                    <span className={classNames('arrow', { expand : expand})} onClick={() => setExpand(!expand)}></span>
                 </div>
                 <div className="oneLineOverview">
                     <div className="pay">
@@ -42,7 +44,7 @@ const DailyBill = ({date,billList}) => {
                 </div>
             </div>
             {/* 单日列表 */}
-            <div className="billList">
+            <div className="billList" style={{ display: expand ? 'block' : 'none' }}>
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
